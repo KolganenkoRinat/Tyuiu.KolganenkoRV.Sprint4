e7ce7368fa7b0503945bf6e9e5a29dcd7732d00b
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-10-28 17:11:13 +0500

    Add project files.

---
+++ Tyuiu.KolganenkoRV.Sprint4.Task0.V3.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KolganenkoRV.Sprint4.Task0.V3.Lib
+{
+    public class DataService : ISprint4Task0V3
+    {
+        public int GetMultOddArrEl(int[] Array)
+        {
+            int p = 1; 
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] % 2 != 0)
+                { 
+                    p = p * Array[i];
+                }
+            
+            }
+            return p;
+        }
+    }
+}
---
+++ Tyuiu.KolganenkoRV.Sprint4.Task0.V3/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.KolganenkoRV.Sprint4.Task0.V3.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт #3 | Выполнила : Колганенко Р. В. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #7                                                              *");
+Console.WriteLine("* Вариант #8                                                              *");
+Console.WriteLine("* Выполнил: Колганенко Ринат Витальевич | ИБКСб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая выводит таблицу значений функции            *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int[] numsArray = {8, 9, 4, 1, 2, 3, 4, 5, 6, 7};
+int p = 1;
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < numsArray.ToArray().Length; i++)
+{
+    Console.WriteLine(numsArray[i]);
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Результирующий массив: ");
+p = ds.GetMultOddArrEl(numsArray);
+Console.WriteLine("Произведение нечетных элементов массива: " + p);
+
+Console.WriteLine();
+Console.ReadKey();
