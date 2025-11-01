98a7336859d57b8bbeb5174f6c134bbc1dab2f70
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-11-01 22:03:42 +0500

    Task2

--- Tyuiu.KolganenkoRV.Sprint4.Task2.V19.Lib/DataService.cs
+++ Tyuiu.KolganenkoRV.Sprint4.Task2.V19.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.KolganenkoRV.Sprint4.Task2.V19.Lib
             int s = 0;
             for (int i = 0; i < array.Length; i++)
             {
-                if ((array[i] % 2 != 0) && (array[i] > 4) && (array[i] < 9))
+                if (array[i] % 2 != 0) 
                 {
                     s += array[i];
                 }
