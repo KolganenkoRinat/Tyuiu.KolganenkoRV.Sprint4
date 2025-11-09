e10939b57e2c8e19fe322767fedf1d1ce37c2c41
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-11-09 22:24:32 +0500

    Task6

---
+++ Tyuiu.KolganenkoRV.Sprint4.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KolganenkoRV.Sprint4.Task6.V4.Lib;
+
+namespace Tyuiu.KolganenkoRV.Sprint4.Task6.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var cities = new string[] { "Москва", "Санкт Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Казахстан", "Самара" };
+            string[] res = ds.Calculate(cities);
+            string[] wait = { "Москва", "Самара" };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KolganenkoRV.Sprint4.Task6.V4/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.KolganenkoRV.Sprint4.Task6.V4.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт #4 | Выполнила : Колганенко Р. В. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #4                                                              *");
+Console.WriteLine("* Выполнил: Колганенко Ринат Витальевич | ИБКСб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая выводит таблицу значений функции            *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+var mas = new string[] { "Москва", "Санкт Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Казахстан", "Самара" };
+
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < mas.Length; i++)
+{
+    Console.WriteLine(mas[i]);
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Результирующий массив: ");
+string[] res = ds.Calculate(mas);
+
+for (int i = 0; i < res.Length; i++)
+{
+    Console.WriteLine(res[i]);
+}
+
+Console.WriteLine();
+Console.ReadKey();
